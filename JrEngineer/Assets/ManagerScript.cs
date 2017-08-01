cc5226ac83aaee0698465d7070c476fef8836783
Nathy-PC\Nathy <ocruznathalia@gmail.com>
2017-08-01 16:18:09 -0300

    Ball Input, GameOverMenu

---
+++ JrEngineer/Assets/ManagerScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerScript : MonoBehaviour {
+    public static bool HasStart;
+    public static bool GameIsOver;
+    public int Score;
+    public Text ScoreTxt;
+    public Animator GameOverAnim;
+
+    // Use this for initialization
+    void Start () {
+        InitialSet();
+    }
+    void InitialSet() {
+        HasStart = false;
+        Score = 0;
+        GameIsOver = false;
+        ScoreTxt.text = Score.ToString();
+    }	
+    void GameOver() {
+        GameIsOver = true;
+        HasStart = true;
+        GameOverAnim.SetTrigger("GameOver");
+    }
+
+    public void RestartGame() {
+        GameOverAnim.SetTrigger("Restart");
+        InitialSet();
+    }
+    void IncrementScore() {
+        Score += 1;
+        ScoreTxt.text = Score.ToString();
+    }
+    void OnEnable()
+    {
+        InputBall.OnCollisionGround += GameOver;
+        InputBall.OnKickBall += IncrementScore;
+    }
+    void OnDisable()
+    {
+        InputBall.OnCollisionGround -= GameOver;
+        InputBall.OnKickBall -= IncrementScore;
+    }
+
+}
---
+++ JrEngineer/Assets/Scripts/InputBall.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputBall :MonoBehaviour
+{
+    public float Impulse;
+    private Rigidbody _rb;
+
+    public delegate void CollisionAction();
+    public static event CollisionAction OnCollisionGround;
+
+    public delegate void ClickAction();
+    public static event ClickAction OnKickBall;
+
+    private int randomForce;
+
+    // Use this for initialization
+    void Start () {
+        _rb=GetComponent<Rigidbody>();
+	}
+	
+    void OnMouseDown() {
+        if (!ManagerScript.GameIsOver) {
+            if (!ManagerScript.HasStart)
+            {
+                ManagerScript.HasStart = true;
+            }
+            _rb.AddForce(transform.up * Impulse);
+            if (OnKickBall != null)
+                OnKickBall();
+        }       
+    }
+
+    void OnMouseEnter() {
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("Ground")) {
+            if (ManagerScript.HasStart) {
+
+                Debug.Log("GameOver");
+                if (OnCollisionGround != null)
+                    OnCollisionGround();
+
+            }
+        }
+    }
+}
